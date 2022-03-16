dd8f0ee9ebeeeef1a2481b6b427bca3b5bd61dd2
Brandon <branrecha140356@gmail.com>
2022-03-15 23:54:27 -0600

    Complete code chocolate

--- Quiz4-5/quiz.cpp
+++ Quiz4-5/quiz.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int costo(int N, int M, int x[], int y[]){
-    int vertical = 0;
-    int horizontal = 0;
+    int columns = 0;
+    int rows = 0;
     for (int i = 0; i < N; i++) {
-        horizontal+= y[i];
+        rows+= y[i];
     }
     for (int j = 0; j < M; j++) {
-        vertical+= x[j];
+        columns+= x[j];
     }
-    int yFirst = horizontal+N*vertical;
-    int xFisrt = vertical+M*horizontal;
-
-    cout<<xFisrt<<endl<<yFirst<<endl;
-return 0;
+    int yFirst = rows+N*columns;
+    int xFisrt = columns+M*rows;
+//    cout<<xFisrt<<endl<<yFirst<<endl;
+return ((yFirst+xFisrt)-abs(yFirst-xFisrt))/2;
 }
 
 int main() {
-    int x[] = {7,5,1,6,0};
     int y[] = {3,4,6,0};
-    costo(4, 5, x, y);
+    int x[] = {7,5,1,6,0};
+    cout<<costo(4, 5, x, y)<<endl;
 return 0;
 }
