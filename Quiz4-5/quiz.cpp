b139447e6c707e2dc135f162a0cc671cc507f8a8
BrandonRetana <branrecha140356@gmail.com>
2022-03-17 14:21:35 -0600

    changes

--- Quiz4-5/quiz.cpp
+++ Quiz4-5/quiz.cpp
@@ -15,16 +15,16 @@ O(n+k)
 Loop invariant:
 En la linea 24 y 27
 Las condiciones del loop invariant son:
-rows <= que la sumatoria desde i = 0 hasta N-1 de y[i]
-columns <= que la sumatoria desde i = 0 hasta M-1 de x[i]
+rows <= que la sumatoria desde i = 0 hasta N-2 de y[i]
+columns <= que la sumatoria desde i = 0 hasta M-2 de x[i]
 */
 int costo(int N, int M, int x[], int y[]){
     int columns = 0;
     int rows = 0;
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < N-1; i++) {
         rows+= y[i];
     }
-    for (int j = 0; j < M; j++) {
+    for (int j = 0; j < M-1; j++) {
         columns+= x[j];
     }
     int yFirst = rows+N*columns;
@@ -34,7 +34,7 @@ return xFirst > yFirst? yFirst: xFirst;
 //--------------------------------------------------------------RadixSort--------------------------------------------------------------
 /* 
 Funcion:
-f(n) = 47m+33mn+9
+f(n) = 47m+33mn+8
 
 O grande:
 O(nk)
@@ -75,10 +75,10 @@ void printArr(int* arr, int n) {
 }
 
 int main() {
-    int y[] = {3,4,6,0};
-    int x[] = {7,5,1,6,0};
+    int y[] = {7,5,1,6};
+    int x[] = {3,4,6};
     cout<<"Costo:\n"<<"x = 7,5,1,6,0\n"<<"y = 3,4,6,0\n"<<"Salida: "<<endl;
-    cout<<costo(4, 5, x, y)<<endl;
+    cout<<costo(5, 4, x, y)<<endl;
     
     cout<<"RadixSort:\n"<<"arr = 201,10,132,55,7 \n"<<"Salida:"<<endl;
     int* arr = new int[5];
@@ -87,7 +87,6 @@ int main() {
     arr[2] = 132;
     arr[3] = 55;
     arr[4] = 7;
-   printArr(arr,5);
    radixSort(arr,5,3);
    printArr(arr,5);
 return 0;
