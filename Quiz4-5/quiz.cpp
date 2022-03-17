98ebd2ed7195b1e8fb5878aad440537b51faf841
BrandonRetana <branrecha140356@gmail.com>
2022-03-16 23:22:41 -0600

    quiz.cpp finalizado

--- Quiz4-5/quiz.cpp
+++ Quiz4-5/quiz.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
 #include <cmath>
+#include<list>
 
 using namespace std;
 
+//--------------------------------------------------------------Costo--------------------------------------------------------------
+/* 
+Funcion:
+f(n) = 6n+6k+12
+
+O grande:
+O(n+k)
+
+Loop invariant:
+En la linea 17 y 18
+Las condiciones del loop invariant son:
+rows <= que la sumatoria desde i = 0 hasta N-1 de y[i]
+columns <= que la sumatoria desde i = 0 hasta M-1 de x[i]
+*/
 int costo(int N, int M, int x[], int y[]){
     int columns = 0;
     int rows = 0;
@@ -13,13 +28,69 @@ int costo(int N, int M, int x[], int y[]){
         columns+= x[j];
     }
     int yFirst = rows+N*columns;
-    int xFisrt = columns+M*rows;
-return xFisrt > yFirst? yFirst: xFisrt;
+    int xFirst = columns+M*rows;
+return xFirst > yFirst? yFirst: xFirst;
+}
+//--------------------------------------------------------------RadixSort--------------------------------------------------------------
+/* 
+Funcion:
+f(n) = 14n(584k+12)
+
+O grande:
+O(nk)
+
+Loop invariant:
+En la linea 62
+Las condiciones del loop invariant son:
+count <= N
+*/
+void radixSort(int *arr, int n, int max) {
+   int i, j, m, p = 1, index, temp, count = 0;
+   list<int> pocket[10];      //radix of decimal number is 10
+   for(i = 0; i< max; i++) {
+      m = pow(10, i+1);
+      p = pow(10, i);
+      for(j = 0; j<n; j++) {
+         temp = arr[j]%m;
+         index = temp/p;      //find index for pocket array
+         pocket[index].push_back(arr[j]);
+      }
+      count = 0;
+      for(j = 0; j<10; j++) {
+         //delete from linked lists and store to array
+         while(!pocket[j].empty()) {
+            arr[count] = *(pocket[j].begin());
+            pocket[j].erase(pocket[j].begin());
+            count++;
+         }
+      }
+   }
+}
+
+void printArr(int* arr, int n) {
+   for (int i = 0; i < n; i++) {
+      cout<<arr[i]<<" ";
+   }
+   cout<<endl;
 }
 
 int main() {
-    int y[] = {3,4,6,0,1,1,1,1};
+    int y[] = {3,4,6,0};
     int x[] = {7,5,1,6,0};
-    cout<<costo(8, 5, x, y)<<endl;
+    cout<<"Costo:\n"<<"x = 7,5,1,6,0\n"<<"y = 3,4,6,0\n"<<"Salida: "<<endl;
+    cout<<costo(4, 5, x, y)<<endl;
+    
+    cout<<"RadixSort:\n"<<"arr = 201,10,132,55,7 \n"<<"Salida:"<<endl;
+    int* arr = new int[5];
+    arr[0] = 201;
+    arr[1] = 10;
+    arr[2] = 132;
+    arr[3] = 55;
+    arr[4] = 7;
+   printArr(arr,5);
+   radixSort(arr,5,3);
+   printArr(arr,5);
 return 0;
 }
+
+//Si quiere ver con mas claridad la informacion extraida de los algoritmos puede ver PDF adjunto en esta misma carpeta. 
